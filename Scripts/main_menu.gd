ea499cd0cca8f5b703c193550f7a1fe807903e08
Lopez10Hugo <hugolopez@correo.ugr.es>
2025-04-08 21:04:11 +0200

    menu de pausa

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,7 +3,6 @@ extends Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
