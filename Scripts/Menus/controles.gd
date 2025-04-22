f945f03c0a52e5f337578ddaf16f9ed48c482bd2
Lopez10Hugo <hugolopez@correo.ugr.es>
2025-04-22 10:44:33 +0200

    controles arreglado

--- Scripts/Menus/controles.gd
+++ Scripts/Menus/controles.gd
@@ -2,10 +2,13 @@ extends Control
 @onready var rebind_control: rebind_control = $MarginContainer/VBoxContainer2/ScrollContainer/VBoxContainer/Rebind_control
 @onready var options_menu: Control = $".."
 @onready var controles: Control = $"."
+@onready var volver_al_menu: Button = $"MarginContainer/VBoxContainer2/Volver al menu"
 
 func _on_focus_entered() -> void:
 	print('rebind control grabbed focus')
 	rebind_control._on_focus_entered()
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -15,4 +18,9 @@ func _unhandled_input(event: InputEvent) -> void:
 			get_viewport().set_input_as_handled()
 
 func _on_volver_al_menu_pressed() -> void:
-	options_menu.inicial_display()
+	options_menu._inicial_display()
+
+func _on_focus_exited() -> void:
+	if controles.is_visible_in_tree():
+		print('volver al menu grabbed focus')
+		volver_al_menu.grab_focus()
--- Scripts/Menus/rebind_control_boton.gd
+++ Scripts/Menus/rebind_control_boton.gd
@@ -4,7 +4,7 @@ extends Control
 
 @onready var label: Label = $HBoxContainer/Label
 @onready var button: Button = $HBoxContainer/Button
-
+var is_listening_for_input = false
 @export var action_name : String = "mover_derecha_1"
 
 func _ready() -> void:
@@ -16,7 +16,7 @@ func set_action_name() -> void:
 	label.text = "No asignado"
 	
 	match action_name:
-		"mover_derecha_1":
+		"mover_derecha_2":
 			label.text = "Moverse a la derecha"
 		"mover_izquierda_1":
 			label.text = "Moverse a la izquierda"
@@ -28,13 +28,34 @@ func set_action_name() -> void:
 func set_text_key() ->void:
 	var action_events = InputMap.action_get_events(action_name)
 	var action_event = action_events[0]
-	var action_key_code = OS.get_keycode_string(action_event.physical_keycode)
+	var action_key_code = ""
+	
+	if action_event is InputEventKey:
+		action_key_code = OS.get_keycode_string(action_event.physical_keycode)
+		#print("Key code:", action_key_code)
+	elif action_event is InputEventJoypadButton:
+		#print("Joypad button:", action_event.button_index)
+		action_key_code = get_joypad_button_name(action_event.button_index)
 	
 	button.text = "%s" % action_key_code
+
+func get_joypad_button_name(index: int) -> String:
+	match index:
+		JOY_BUTTON_A: return "A"
+		JOY_BUTTON_B: return "B"
+		JOY_BUTTON_X: return "X"
+		JOY_BUTTON_Y: return "Y"
+		JOY_BUTTON_START: return "Start"
+		JOY_BUTTON_DPAD_UP: return "D-Pad Up"
+		JOY_BUTTON_DPAD_DOWN: return "D-Pad Down"
+		JOY_BUTTON_DPAD_LEFT: return "D-Pad Left"
+		JOY_BUTTON_DPAD_RIGHT: return "D-Pad Right"
+		_: return "Botón " + str(index)
 
 func _on_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		button.text = "Presiona alguna tecla..."
+		is_listening_for_input = true
 		set_process_unhandled_key_input(toggled_on)
 		
 		for i in get_tree().get_first_nodes_in_group("hotkey_button"):
@@ -50,8 +71,11 @@ func _on_button_toggled(toggled_on: bool) -> void:
 		set_text_key()
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	rebind_action_key(event)
-	button.button_pressed = false
+	if not is_listening_for_input:
+		pass
+	else:
+		rebind_action_key(event)
+		button.button_pressed = false
 
 func rebind_action_key(evento):
 	InputMap.action_erase_events(action_name)
